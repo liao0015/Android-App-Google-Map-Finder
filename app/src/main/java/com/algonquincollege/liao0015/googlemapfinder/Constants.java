6cc4424b27ad3cda7fdee4de7e0a32914e549f6c
BoLiaoAC <liao0015@algonquinlive.com>
2016-01-06 23:56:01 -0500

    updated v1.0

---
+++ app/src/main/java/com/algonquincollege/liao0015/googlemapfinder/Constants.java
@@ -0,0 +1,12 @@
+package com.algonquincollege.liao0015.googlemapfinder;
+
+/**
+ * Created by boliao on 2015-10-13.
+ */
+public interface Constants {
+    public static final String jazan = "Algonquin Jazan College of Excellence";
+    public static final String pembroke = "Algonquin College in the Ottawa Valley";
+    public static final String perth = "7 Craig Street, Perth, ON, K7H 1X7";
+    public static final String woodroffe = "1385 Woodroffe Avenue, Ottawa, ON, K2G 1V8";
+
+}
